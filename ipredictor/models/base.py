ARV accuracy calculation method error fixed

ipredictor/models/base.py
@@ -195,13 +195,13 @@ class IntervalDataMixin:
 		mse_min = 0
 		mse_avg_max = 0
 		mse_avg_min = 0
-		predicted_mean = np.mean(predicted, axis=0)
+		sample_mean = np.mean(real, axis=0)
 
 		for i in range(min(len(real), len(predicted))):
 			mse_max += (real[i][0] - predicted[i][0]) ** 2
 			mse_min += (real[i][1] - predicted[i][1]) ** 2
-			mse_avg_max += (real[i][0] - predicted_mean[0]) ** 2
-			mse_avg_min += (real[i][1] - predicted_mean[1]) ** 2
+			mse_avg_max += (real[i][0] - sample_mean[0]) ** 2
+			mse_avg_min += (real[i][1] - sample_mean[1]) ** 2
 		return float((mse_max + mse_min) / (mse_avg_max + mse_avg_min))
 
 

ipredictor/tests/test_base.py
@@ -80,4 +80,4 @@ class BaseModelTestCase(unittest.TestCase):
 		model = IntervalDataMixin()
 		x = [np.array([[i+3], [i+2]]) for i in range(5)]
 		y = [np.array([[i+1], [i+1]]) for i in range(5)]
-		self.assertEqual(model.arv(x, y), 25/45)
+		self.assertEqual(model.arv(x, y), 1.25)