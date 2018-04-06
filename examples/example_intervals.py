41e2327174e257faf155b8c659a35f85cfb172cb
zenio <i@ufasg.com>
2018-04-06 10:41:59 +0500

    Data samples and usage examples added

--- examples/example_intervals.py
+++ examples/example_intervals.py
@@ -1,25 +1,38 @@
 #: -*- coding: utf-8 -*-
 import logging
 
-from ipredictor.models import HoltWintersI
-from ipredictor.tools import data_reader
+from ipredictor.models import HoltWintersI, ANNI, HybridI, LSTMI, HoltI, HoltWinters, ANN, HybridIPoints
+from ipredictor import tools
 from ipredictor.plotter import Plotter
+
+import numpy as np
+
+np.random.seed(7)
 
 
 if __name__ == "__main__":
 	logging.basicConfig(level=logging.DEBUG)
-	data = data_reader('../ipredictor/tests/assets/intervals.csv',
-	                   intervals=True)
-	train, test = data[:-5], data[-5:]
-	model = HoltWintersI(train, season_period=2)
-	prediction = model.predict(steps=5)
 
-	plotter = Plotter()
-	plotter.add(train, color='b')
-	plotter.add(test, color='g')
-	plotter.add(prediction, color='r')
-	plot = plotter.prepare()
-	plot.show()
+	season_period = 24
+	predict = season_period
+
+	data = tools.data_reader('datasamples/25.csv', intervals=True, resample=True)
+	train, test = data[:-predict], data[-predict:]
+
+	model = HoltWintersI(train, season_period=season_period)
+	prediction = model.predict(steps=predict)
+
+	print "MSE: ", HoltWintersI.mse(test, prediction)
+	print "MAPE: ", HoltWintersI.mape(test, prediction)
+	print "ARV: ", HoltWintersI.arv(test, prediction, train)
+	plotter = Plotter(rows=1, cols=1)
+
+	plotter.add(train, color='b', label=u'Raw')
+	plotter.add(test, color='g', label=u'Validation')
+	plotter.add(prediction, color='r', label=u'Prediction')
+	plotter.prepare(pos=0, title=u"", xlabel=u"Time", ylabel=u"Gas pressure")
+	plotter.show()
+
 
 
 
